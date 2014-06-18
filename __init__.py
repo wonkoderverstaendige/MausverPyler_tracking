9cf4bc91a5a5a04b9125c11100926faa213bc471
Ronny Eichler <ronny.eichler@gmail.com>
2014-06-18 19:04:41 +0200

    Add missing __init__.py files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 18/06/14 19:01
+@author: <'Ronny Eichler'> ronny.eichler@gmail.com
+
+"""
---
+++ playground/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 18/06/14 19:00
+@author: <'Ronny Eichler'> ronny.eichler@gmail.com
+
+"""
